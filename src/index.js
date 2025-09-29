62cc8d3d5aa89384be1f1a2f2825eb5ccc0ff5e5
jaylin <jaylinhernandez123@icloud.com>
2025-09-29 14:18:50 -0400

    working on the ship fucniton'

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ export function fun() {
 export function read() {
   return 3;
 }
+
+export class ship {
+  constructor(id, length, hit, sunk) {
+    (this.id = id), (this.length = length.fun);
+    (this.hit = hit), (this.sunk = sunk);
+  }
+  length() {
+    return this.length;
+  }
+}
+let shipTypes = { Carrier: 4, battleship: 4 };
+let testing = new ship("fsafd");
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,18 @@
 import { fun, read } from "./index";
+import { ship } from "./index";
 test("first function", () => {
   expect(fun()).toBe(2);
 });
 test("second", () => {
   expect(read()).toBe(3);
+});
+
+describe("Ship functins", () => {
+  let Carrier;
+  beforeEach(() => {
+    Carrier = new ship("Carreir", 5);
+  });
+  it("name of ship", () => {
+    expect(Carrier.length).toBe(5);
+  });
 });
